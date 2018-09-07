7419e75926203ea30bcfd79bc7b53b11aba78a86
Ingvar <shymanski.ihar@gmail.com>
2018-09-07 03:32:22 +0300

    Got trace information about target method

--- MainApp/MainApp/Program.cs
+++ MainApp/MainApp/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Threading;
+using System.Diagnostics;
 
 namespace MainApp
 {
@@ -11,6 +13,10 @@ namespace MainApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Tracer tracer = new Tracer();
+            tracer.StartTrace();
+            Console.ReadKey();
         }
     }
 
@@ -38,7 +44,16 @@ namespace MainApp
 
         public void StartTrace()
         {
-
+            StackTrace trace = new StackTrace();
+            Console.WriteLine(trace.GetFrame(1).GetMethod().ReflectedType.ToString());
+            Console.WriteLine(trace.GetFrame(1).GetMethod().ToString());
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            int a = 6;
+            int b = a * 90;
+            int c = b * 60;
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedTicks * ((1000L * 1000L * 1000L) / Stopwatch.Frequency));
         }
 
         public void StopTrace()
